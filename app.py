32e19842186d153aa0a01f06fd3a2736e6743f0e
yourusername <youremail@example.com>
2026-04-25 14:55:44 +0530

    fix app entrypoint

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import gradio as gr
 from dashboard import demo
 
-demo.launch(server_name="0.0.0.0", server_port=7860)
+if __name__ == "__main__":
+    demo.launch(server_name="0.0.0.0", server_port=7860)
